2426446c145641543ad411efad80133590875f7d
abdalla lahwani <abdalla.lahwani@gmail.com>
2025-02-06 12:14:07 +0200

    initial_code

---
+++ pcc_client.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+
+#define BUFFER_SIZE 1048576 // 1MB
+
+ssize_t write_all(int fd, const void *buf, size_t count) {
+    size_t total_written = 0;
+    while (total_written < count) {
+        ssize_t written = write(fd, buf + total_written, count - total_written);
+        if (written < 0) {
+            if (errno == EINTR) continue;
+            return -1;
+        }
+        if (written == 0) return total_written;
+        total_written += written;
+    }
+    return total_written;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <server_ip> <port> <file_path>\n", argv[0]);
+        exit(1);
+    }
+
+    const char *server_ip = argv[1];
+    unsigned short port = (unsigned short)atoi(argv[2]);
+    const char *file_path = argv[3];
+
+    int file_fd = open(file_path, O_RDONLY);
+    if (file_fd < 0) {
+        perror("open");
+        exit(1);
+    }
+
+    struct stat st;
+    if (fstat(file_fd, &st) < 0) {
+        perror("fstat");
+        exit(1);
+    }
+
+    uint32_t file_size = st.st_size;
+    if ((off_t)file_size != st.st_size) {
+        fprintf(stderr, "File size exceeds 32 bits\n");
+        exit(1);
+    }
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <= 0) {
+        perror("inet_pton");
+        exit(1);
+    }
+
+    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("connect");
+        exit(1);
+    }
+
+    uint32_t file_size_net = htonl(file_size);
+    if (write_all(sockfd, &file_size_net, sizeof(file_size_net)) != sizeof(file_size_net)) {
+        perror("write N");
+        exit(1);
+    }
+
+    char buffer[BUFFER_SIZE];
+    ssize_t total_sent = 0;
+    while (total_sent < file_size) {
+        ssize_t bytes_read = read(file_fd, buffer, sizeof(buffer));
+        if (bytes_read < 0) {
+            perror("read");
+            exit(1);
+        }
+        if (bytes_read == 0) {
+            fprintf(stderr, "Unexpected EOF\n");
+            exit(1);
+        }
+        if (write_all(sockfd, buffer, bytes_read) != bytes_read) {
+            perror("write data");
+            exit(1);
+        }
+        total_sent += bytes_read;
+    }
+
+    uint32_t count_net;
+    if (read(sockfd, &count_net, sizeof(count_net)) != sizeof(count_net)) {
+        perror("read count");
+        exit(1);
+    }
+
+    uint32_t count = ntohl(count_net);
+    printf("# of printable characters: %u\n", count);
+
+    close(sockfd);
+    close(file_fd);
+    return 0;
+}
---
+++ pcc_server.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <errno.h>
+
+#define BUFFER_SIZE 1048576 // 1MB
+#define PRINTABLE_START 32
+#define PRINTABLE_END 126
+#define PRINTABLE_COUNT (PRINTABLE_END - PRINTABLE_START + 1)
+
+static volatile sig_atomic_t should_exit = 0;
+static uint32_t pcc_total[PRINTABLE_COUNT] = {0};
+
+void sigint_handler(int sig) {
+    should_exit = 1;
+}
+
+ssize_t read_all(int fd, void *buf, size_t count) {
+    size_t total_read = 0;
+    while (total_read < count) {
+        ssize_t bytes_read = read(fd, buf + total_read, count - total_read);
+        if (bytes_read <= 0) {
+            return bytes_read;
+        }
+        total_read += bytes_read;
+    }
+    return total_read;
+}
+
+void print_stats() {
+    for (int i = 0; i < PRINTABLE_COUNT; i++) {
+        char c = i + PRINTABLE_START;
+        printf("char '%c' : %u times\n", c, pcc_total[i]);
+    }
+}
+
+void process_client(int client_fd) {
+    uint32_t n_net;
+    if (read_all(client_fd, &n_net, sizeof(n_net)) != sizeof(n_net)) {
+        perror("read N");
+        close(client_fd);
+        return;
+    }
+    uint32_t n = ntohl(n_net);
+
+    uint32_t current_count = 0;
+    uint32_t current_stats[PRINTABLE_COUNT] = {0};
+    char buffer[BUFFER_SIZE];
+    size_t total_read = 0;
+
+    while (total_read < n) {
+        size_t remaining = n - total_read;
+        size_t to_read = remaining < sizeof(buffer) ? remaining : sizeof(buffer);
+        ssize_t bytes_read = read(client_fd, buffer, to_read);
+
+        if (bytes_read <= 0) {
+            if (bytes_read == 0) {
+                fprintf(stderr, "Client closed connection prematurely\n");
+            } else if (errno == ETIMEDOUT || errno == ECONNRESET || errno == EPIPE) {
+                perror("read error");
+            } else {
+                perror("read");
+                exit(1);
+            }
+            close(client_fd);
+            return;
+        }
+
+        for (ssize_t i = 0; i < bytes_read; i++) {
+            unsigned char c = buffer[i];
+            if (c >= PRINTABLE_START && c <= PRINTABLE_END) {
+                current_count++;
+                current_stats[c - PRINTABLE_START]++;
+            }
+        }
+        total_read += bytes_read;
+    }
+
+    uint32_t count_net = htonl(current_count);
+    if (write(client_fd, &count_net, sizeof(count_net)) != sizeof(count_net)) {
+        perror("write count");
+        close(client_fd);
+        return;
+    }
+
+    for (int i = 0; i < PRINTABLE_COUNT; i++) {
+        pcc_total[i] += current_stats[i];
+    }
+
+    close(client_fd);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    unsigned short port = (unsigned short)atoi(argv[1]);
+
+    struct sigaction sa;
+    sa.sa_handler = sigint_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGINT, &sa, NULL) < 0) {
+        perror("sigaction");
+        exit(1);
+    }
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+    int optval = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0) {
+        perror("setsockopt");
+        exit(1);
+    }
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(port);
+
+    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind");
+        exit(1);
+    }
+
+    if (listen(sockfd, 10) < 0) {
+        perror("listen");
+        exit(1);
+    }
+
+    while (!should_exit) {
+        int client_fd = accept(sockfd, NULL, NULL);
+        if (client_fd < 0) {
+            if (errno == EINTR && should_exit) {
+                break;
+            }
+            perror("accept");
+            continue;
+        }
+        process_client(client_fd);
+    }
+
+    close(sockfd);
+    print_stats();
+    return 0;
+}
